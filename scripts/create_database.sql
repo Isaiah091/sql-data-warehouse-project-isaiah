a903596679d839e699579e52a217e680c563b4c8
Isaiah091 <isaiahobey1999@gmail.com>
2025-07-13 09:08:03 -0400

    Update create_database.sql

--- scripts/create_database.sql
+++ scripts/create_database.sql
@@ -16,29 +16,26 @@ ALL data will be PERMENATLY deleted. Ensure you have proper backups before runni
 
 
 
-USE master;
-GO
 
-IF EXISTS (select 1 from sys.databases WHERE name = 'DataWarehouse') -- Check if the Data Warehouse exists. If true it will return 1
+
+IF EXISTS(select 1 from sys.databases where name = 'DataWarehouse')
 	BEGIN
-	/*
-	--SINGLE USER ensures that users already connected to the DB to not get disconnected when the Database is altered
-	--When you run the ALTER DATABASE SET SINGLE_USER statement, it does not complete because the users are still connected to it.
-	--WITH ROLLBACK IMMEDIATE: This option will rollback all the open transactions. It does not wait for the transaction to complete
-	--WITH NO_WAIT: this will rollback the transactions only once they complete. If they do not complete, the ALTER DATABASE statements fails
-	*/
+	USE master;
 
 		ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE; --Rollback any transactions before Altering the DB
 		DROP DATABASE DataWarehouse;
-	END;
 
+		CREATE DATABASE DataWarehouse;
+	
+	END
+ELSE
+	BEGIN
+		CREATE DATABASE DataWarehouse;
+		select name, 1 from sys.databases
+	END
+-------------------------------------- End of Creating Database--------------------------------
 GO
-
-	--Create the DataWarehouse DB
-	CREATE DATABASE DataWarehouse;
-	GO
-
-	--Create Schemas
+--Create Schemas
 	USE DataWarehouse;
 	
 	GO
